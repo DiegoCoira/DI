3b4b8a535c61d3276eeef35b985b724b239efe34
Diego Coira <dcoirap@fpcoruna.afundacion.org>
2023-10-09 17:38:00 +0200

    Todo acabado :)

--- sprint1Tkinter/catalog/cell.py
+++ sprint1Tkinter/catalog/cell.py
@@ -4,10 +4,11 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 
 class CatalogCell:
-    def __init__(self, title, path):
-        
+    def __init__(self, title, path, description):
         self.title = title
         self.path = path
+        self.description = description
+
         image = Image.open(self.path)
         image = image.resize((150, 150), Image.LANCZOS) 
         self.image_tk = ImageTk.PhotoImage(image)
--- sprint1Tkinter/catalog/window.py
+++ sprint1Tkinter/catalog/window.py
@@ -3,22 +3,24 @@ import tkinter as tk
 from cell import CatalogCell
 from tkinter import messagebox
 from PIL import Image, ImageTk
+from detail_window import DetailWindow
 
 class MainWindow():
 
     def on_button_clicked(self, cell):
-        message = "Has hecho click en la celda" + cell.title
-        messagebox.showinfo("Informacion", message)
+        detail_window = DetailWindow(self.root, cell.title, cell.image_tk, cell.description)
+
 
     def __init__(self,root):
         root.title("MainWindow")
+        self.root = root 
 
         self.cells = [
-            CatalogCell("BoyDinner", "catalog/data/unedited/BoyDinner.jpg"),
-            CatalogCell("mi amigo dice", "catalog/data/unedited/mi amigo dice.jpg"),
-            CatalogCell("Napoleon", "catalog/data/unedited/Napoleon.jpg"),
-            CatalogCell("Tenemos", "catalog/data/unedited/Tenemos.png"),
-            CatalogCell("We love we live we", "catalog/data/unedited/We love we live we.jpg")
+            CatalogCell("BoyDinner", "catalog/data/unedited/BoyDinner.jpg","Average boy dinner, whatching Sam Sulek and having an awesome dinner"),
+            CatalogCell("mi amigo dice", "catalog/data/unedited/mi amigo dice.jpg", "Mi amigo dice meme, increible meme (no o vin na miña vida, encontreino en google)"),
+            CatalogCell("Napoleon", "catalog/data/unedited/Napoleon.jpg","No hay nada que podamos hacer, meme de napoleon saleume 32410253 veces en Tiktok"),
+            CatalogCell("Tenemos", "catalog/data/unedited/Tenemos.png", "Tipico meme comunista, bastante gracioso con utilida e saleume en tiktok"),
+            CatalogCell("We love we live we", "catalog/data/unedited/We love we live we.jpg", "Mi compa el шайлушай, meme cansino saleu 20419439  veces")
         ]
 
         for i, cell in enumerate(self.cells):
