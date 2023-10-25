78704f5c05d5b7e7f728e55bccffdc80a6df5b06
Diego Coira <dcoirap@fpcoruna.afundacion.org>
2023-10-25 09:01:14 +0200

    Tarea #7: Completada

--- Sprint2Catalog/window.py
+++ Sprint2Catalog/window.py
@@ -1,6 +1,6 @@
 # Import necessary modules and classes from tkinter, PIL, and your custom modules
 from io import BytesIO
-from tkinter import Label, ttk
+from tkinter import Canvas, Frame, Label, Scrollbar, messagebox
 import tkinter as tk
 from cell import CatalogCell
 from tkinter import messagebox
@@ -29,8 +29,14 @@ class MainWindow():
       root.title("MainWindow")
       self.root = root
 
-      self.cell_frame = tk.Frame(self.root)
-      self.cell_frame.grid(row=0, column=0, padx=10, pady=10)
+      self.canvas = Canvas(root)
+      self.scrollbar = Scrollbar(root, orient="vertical", command=self.canvas.yview) 
+      self.scrollable_frame = Frame(self.canvas)  
+
+      self.scrollable_frame.bind("<Configure>",lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+    
+      self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw") ## Creamos la ventana en la posición 0, 0. La ancla es para que haya un ajuste del marco.
+      self.canvas.configure(yscrollcommand=self.scrollbar.set) ## Configura el lienzo para emplearlo.
 
       self.cell_list = [] 
       for i, data in enumerate(json_data):
@@ -42,9 +48,21 @@ class MainWindow():
           cell = CatalogCell(name, descripcion, url, imagen) 
           self.cell_list.append(cell)
 
-          label = Label(self.cell_frame, image=cell.imagen, text=name, compound=tk.BOTTOM)
-          label.grid(row=i, column=0, sticky="nsew")
-          label.bind("<Button-1>", lambda event, cell=cell: self.on_button_clicked(cell))
+          self.canvas.grid(row=0, column=0, sticky="nsew") ## Para colocar el lienzo:
+          self.scrollbar.grid(row=0, column=1, sticky="ns") ## Para colocar la barra:
+          
+          root.grid_rowconfigure(0, weight=1)
+          root.grid_columnconfigure(0, weight=1)
+          
+          frame = Frame(self.scrollable_frame) ## Marco que contendrá lo que se vaya a mostrar
+          frame.pack(pady=10)
+          
+          label = Label(frame, image=cell.imagen, text=name, compound=tk.BOTTOM) ## Etiqueta para mostrar la imagen
+          label.grid(row=0, column=0)
+          
+          label.bind("<Button-1>", lambda event, cell=cell: self.on_button_clicked(cell))    
+    
+    
 
       for i in range(len(self.cell_list)):
           self.cell_frame.grid_rowconfigure(i, weight=1)
@@ -53,10 +71,10 @@ class MainWindow():
           root.geometry(f"+{int(x)}+{int(y)}")
 
       
-      barra_menus = tk.Menu() ## Instanciamos la barra
-      menu_archivo = tk.Menu(barra_menus, tearoff=False) ## 
+      barra_menus = tk.Menu()
+      menu_archivo = tk.Menu(barra_menus, tearoff=False) 
         
-      menu_archivo.add_command(label="Acerca de", command=self.on_button_clicked2) ##
+      menu_archivo.add_command(label="Acerca de", command=self.on_button_clicked2) 
       barra_menus.add_cascade(menu=menu_archivo, label="Ayuda")
       root.config(menu=barra_menus)
 
