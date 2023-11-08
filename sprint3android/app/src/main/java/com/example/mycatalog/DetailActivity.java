e5422cd9d354ec057b87cfbcd8b8e760fd9abd5b
Diego Coira <dcoirap@fpcoruna.afundacion.org>
2023-11-08 10:44:08 +0100

    Sprint 3, Tarea 4 Completada

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -2,6 +2,7 @@ package com.example.mycatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
@@ -9,6 +10,6 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_detail); 
     }
 }
