62777f1810437cdbc6a3ebcde5fbd3bc6bb172be
DiegoCoira <114064760+DiegoCoira@users.noreply.github.com>
2023-11-09 20:59:38 +0100

    Comentarios añadidos a AboutFragment.java

--- sprint3android/app/src/main/java/com/example/mycatalog/AboutFragment.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/AboutFragment.java
@@ -1,27 +1,27 @@
 package com.example.mycatalog;
 
 import android.os.Bundle;
-
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class AboutFragment extends Fragment {
-    public static AboutFragment newInstance() {  // Creación de nueva instancia de AboutFragment
+
+    // Método estático para crear una nueva instancia de AboutFragment
+    public static AboutFragment newInstance() {
         AboutFragment frag = new AboutFragment();
         return frag;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
-    Bundle savedInstanceState) { // Creación y devolución de la vista del fragmento
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Este método es llamado cuando el fragmento debe crear su vista.
+        // Inflamos el diseño del fragmento (fragment_about.xml) en la vista del mismo.
         View layout = inflater.inflate(R.layout.fragment_about, container, false);
-        // Inflamos el diseño del fragmento en la vista del mismo.
-        return layout;
+        return layout; // Devolvemos la vista inflada.
     }
 }
